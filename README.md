88d8338423c36c29d7794d239e1d4e92e6a8be95
Jake Boone <jakeboone02@gmail.com>
2015-03-18 08:22:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# format-quantity
+
+### Installation:
+
+```
+npm i --save format-quantity
+```
+
+### Usage:
+
+```
+var formatQuantity = require("format-quantity");
+
+console.log( formatQuantity(1.5) );   // "1 1/2"
+console.log( formatQuantity(2.66) );  // "2 2/3"
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+function closeEnough (a, b) {
+  return Math.abs(a - b) < 0.009;
+}
+
+module.exports = function(sInQty) {
+
+  sQty = sInQty;
+
+  // bomb out if not a number
+  if (isNaN(sQty)) {
+
+    return "-1";
+  }
+
+  var dQty = sQty - 0;
+
+  if (dQty === 0) {
+
+    return "";
+  }
+
+  var iFloor = Math.floor(dQty);
+  var sFloor = (iFloor === 0.0 ? "" : iFloor + " ");
+  var dDecimal = dQty - iFloor;
+
+  // Handle integers first.  Just return the given value.
+  if (dDecimal === 0) {
+
+    return sInQty + "";
+  }
+
+  // Handle infinitely repeating decimals next, since
+  // we'll never get an exact match for a switch case:
+  if (closeEnough(dDecimal, 0.33)) {
+
+    sQty = sFloor + "1/3";
+
+  } else if (closeEnough(dDecimal, 0.66)) {
+
+    sQty = sFloor + "2/3";
+
+  } else if (closeEnough(dDecimal, 0.2)) {
+
+    sQty = sFloor + "1/5";
+
+  } else if (closeEnough(dDecimal, 0.4)) {
+
+    sQty = sFloor + "2/5";
+
+  } else if (closeEnough(dDecimal, 0.6)) {
+
+    sQty = sFloor + "3/5";
+
+  } else if (closeEnough(dDecimal, 0.8)) {
+
+    sQty = sFloor + "4/5";
+
+  } else {
+
+    switch (dDecimal) {
+      case 0.125: sQty = sFloor + "1/8"; break;
+      // case 0.2:   sQty = sFloor + "1/5"; break;
+      case 0.25:  sQty = sFloor + "1/4"; break;
+      case 0.375: sQty = sFloor + "3/8"; break;
+      // case 0.4:   sQty = sFloor + "2/5"; break;
+      case 0.5:   sQty = sFloor + "1/2"; break;
+      // case 0.6:   sQty = sFloor + "3/5"; break;
+      case 0.625: sQty = sFloor + "5/8"; break;
+      case 0.75:  sQty = sFloor + "3/4"; break;
+      // case 0.8:   sQty = sFloor + "4/5"; break;
+      case 0.875: sQty = sFloor + "7/8"; break;
+    }
+  }
+
+  return sQty + "";
+}
