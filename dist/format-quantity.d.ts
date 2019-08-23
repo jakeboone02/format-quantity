feed43d85430872b4846e0cec7c120353a0fa0dd
Jake Boone <jakeboone02@gmail.com>
2019-08-23 15:18:34 -0700

    Ignore dist folder

--- dist/format-quantity.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Formats a number (or string that appears to be a number)
- * as one would see it written in imperial measurements, e.g.
- * "1 1/2" instead of "1.5".
- */
-declare function formatQuantity(qty: string | number): string;
-export default formatQuantity;
