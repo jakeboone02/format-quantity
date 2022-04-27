b2bb567c79e8ac585a330becf6aeffa79a8406d5
Jake Boone <jakeboone02@gmail.com>
2022-04-26 18:34:12 -0700

    Accept options object and handle more fractions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,26 +1,49 @@
 import formatQuantity from '.';
 
-it('works', () => {
-  // NaN
+it('returns null for NaN and non-numeric strings', () => {
+  expect(formatQuantity(NaN)).toBe(null);
   expect(formatQuantity('NaN')).toBe(null);
-  // Zero should return blank string
+});
+it('returns blank string for zero', () => {
   expect(formatQuantity(0)).toBe('');
-  // Integers
+});
+it('handles integers', () => {
   expect(formatQuantity(1)).toBe('1');
   expect(formatQuantity(1, true)).toBe('1');
   expect(formatQuantity(-1)).toBe('-1');
   expect(formatQuantity(100)).toBe('100');
-  // Most decimal values should be returned as-is
-  expect(formatQuantity(1.1)).toBe('1.1');
-  expect(formatQuantity(-1.1)).toBe('-1.1');
-  // Quarters
+});
+it('returns most decimal values as-is', () => {
+  expect(formatQuantity(1.123)).toBe('1.123');
+  expect(formatQuantity(-1.123)).toBe('-1.123');
+});
+it('handles halves', () => {
+  expect(formatQuantity(1.51)).toBe('1.51');
+  expect(formatQuantity(1.5)).toBe('1 1/2');
+  expect(formatQuantity(1.5, true)).toBe('1½');
+  expect(formatQuantity(1.52)).toBe('1.52');
+});
+it('handles thirds', () => {
+  expect(formatQuantity(1.32)).toBe('1.32');
+  expect(formatQuantity(1.33)).toBe('1 1/3');
+  expect(formatQuantity(1.33, true)).toBe('1⅓');
+  expect(formatQuantity(1.3333333333333333)).toBe('1 1/3');
+  expect(formatQuantity(1.34)).toBe('1.34');
+  expect(formatQuantity(1.66)).toBe('1 2/3');
+  expect(formatQuantity(1.66, true)).toBe('1⅔');
+  expect(formatQuantity(1.667)).toBe('1 2/3');
+  expect(formatQuantity(1.6666666666666666)).toBe('1 2/3');
+  expect(formatQuantity(1.67)).toBe('1.67');
+});
+it('handles quarters', () => {
   expect(formatQuantity(1.25)).toBe('1 1/4');
   expect(formatQuantity(1.25, true)).toBe('1¼');
   expect(formatQuantity(-1.25)).toBe('-1 1/4');
   expect(formatQuantity(1.75)).toBe('1 3/4');
   expect(formatQuantity(1.75, true)).toBe('1¾');
   expect(formatQuantity(-1.75)).toBe('-1 3/4');
-  // Fifths
+});
+it('handles fifths', () => {
   expect(formatQuantity(0.2)).toBe('1/5');
   expect(formatQuantity(0.2, true)).toBe('⅕');
   expect(formatQuantity(1.2)).toBe('1 1/5');
@@ -34,23 +57,18 @@ it('works', () => {
   expect(formatQuantity(0.8)).toBe('4/5');
   expect(formatQuantity(1.8)).toBe('1 4/5');
   expect(formatQuantity(1.8, true)).toBe('1⅘');
-  // Thirds
-  expect(formatQuantity(1.32)).toBe('1.32');
-  expect(formatQuantity(1.33)).toBe('1 1/3');
-  expect(formatQuantity(1.33, true)).toBe('1⅓');
-  expect(formatQuantity(1.3333333333333333)).toBe('1 1/3');
-  expect(formatQuantity(1.34)).toBe('1.34');
-  expect(formatQuantity(1.66)).toBe('1 2/3');
-  expect(formatQuantity(1.66, true)).toBe('1⅔');
-  expect(formatQuantity(1.667)).toBe('1 2/3');
-  expect(formatQuantity(1.6666666666666666)).toBe('1 2/3');
-  expect(formatQuantity(1.67)).toBe('1.67');
-  // Halves
-  expect(formatQuantity(1.51)).toBe('1.51');
-  expect(formatQuantity(1.5)).toBe('1 1/2');
-  expect(formatQuantity(1.5, true)).toBe('1½');
-  expect(formatQuantity(1.52)).toBe('1.52');
-  // Eighths
+});
+it('handles sixths', () => {
+  expect(formatQuantity(1 + 1 / 6)).toBe('1 1/6');
+  expect(formatQuantity(1.166, true)).toBe('1⅙');
+  expect(formatQuantity(1 + 5 / 6)).toBe('1 5/6');
+  expect(formatQuantity(1.833, true)).toBe('1⅚');
+});
+it('handles sevenths', () => {
+  expect(formatQuantity(1 + 1 / 7)).toBe('1 1/7');
+  expect(formatQuantity(1.1428, true)).toBe('1⅐');
+});
+it('handles eighths', () => {
   expect(formatQuantity(1.125)).toBe('1 1/8');
   expect(formatQuantity(1.125, true)).toBe('1⅛');
   expect(formatQuantity(1.375)).toBe('1 3/8');
@@ -59,4 +77,17 @@ it('works', () => {
   expect(formatQuantity(1.625, true)).toBe('1⅝');
   expect(formatQuantity(1.875)).toBe('1 7/8');
   expect(formatQuantity(1.875, true)).toBe('1⅞');
+});
+it('handles ninths', () => {
+  expect(formatQuantity(1 + 1 / 9)).toBe('1 1/9');
+  expect(formatQuantity(1.11111, true)).toBe('1⅑');
+});
+it('handles tenths', () => {
+  expect(formatQuantity(1 + 1 / 10)).toBe('1 1/10');
+  expect(formatQuantity(1.1, true)).toBe('1⅒');
+});
+it('handles options object', () => {
+  expect(formatQuantity(1.5, {})).toBe('1 1/2');
+  expect(formatQuantity(1.5, { vulgarFractions: false })).toBe('1 1/2');
+  expect(formatQuantity(1.5, { vulgarFractions: true })).toBe('1½');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,74 @@
+export interface FormatQuantityOptions {
+  /**
+   * Output unicode vulgar fractions--like "½" instead of "1/2"--when appropriate.
+   */
+  vulgarFractions?: boolean;
+}
+
+export type VulgarFraction =
+  | '⅓'
+  | '⅔'
+  | '⅕'
+  | '⅖'
+  | '⅗'
+  | '⅘'
+  | '⅛'
+  | '¼'
+  | '⅜'
+  | '½'
+  | '⅝'
+  | '¾'
+  | '⅞'
+  | '⅐'
+  | '⅑'
+  | '⅒'
+  | '⅙'
+  | '⅚';
+
 /**
  * Determines if two numbers are close enough to consider
- * them equal for our purposes
+ * them equal for the purposes of this package.
  */
 const closeEnough = (a: number, b: number) => Math.abs(a - b) < 0.009;
 
 /**
+ * A map of unicode vulgar fractions to their traditional equivalents.
+ */
+export const vulgarToPlainMap: Record<VulgarFraction, `${number}/${number}`> = {
+  '⅓': '1/3',
+  '⅔': '2/3',
+  '⅕': '1/5',
+  '⅖': '2/5',
+  '⅗': '3/5',
+  '⅘': '4/5',
+  '⅛': '1/8',
+  '¼': '1/4',
+  '⅜': '3/8',
+  '½': '1/2',
+  '⅝': '5/8',
+  '¾': '3/4',
+  '⅞': '7/8',
+  '⅐': '1/7',
+  '⅑': '1/9',
+  '⅒': '1/10',
+  '⅙': '1/6',
+  '⅚': '5/6',
+};
+
+/**
  * Formats a number (or string that appears to be a number)
  * as one would see it written in imperial measurements, e.g.
- * "1 1/2" instead of "1.5".  To use vulgar fractions, e.g. "½",
- * pass `true` as the second argument.
+ * "1 1/2" instead of "1.5". To use unicode vulgar fraction
+ * characters like "½", pass `true` or `{vulgarFractions: true}`
+ * as the second argument.
  */
-function formatQuantity(qty: string | number, useVulgarFractions?: boolean) {
+function formatQuantity(
+  qty: string | number,
+  options?: boolean | FormatQuantityOptions
+) {
   const dQty = typeof qty === 'string' ? parseFloat(qty) : qty;
 
-  // Bomb out if not a number
+  // Return `null` if input is not number-like
   if (isNaN(dQty) || dQty === null) {
     return null;
   }
@@ -28,43 +83,60 @@ function formatQuantity(qty: string | number, useVulgarFractions?: boolean) {
   const sFloor = iFloor === 0.0 ? '' : `${dQty < 0 ? '-' : ''}${iFloor} `;
   const dDecimal = dQtyAbs - iFloor;
 
-  // Handle integers first.  Just return the given value as a string.
+  // For integers just return the given value as a string
   if (dDecimal === 0) {
     return `${dQty}`;
   }
 
+  const useVulgarFractions =
+    options === true ||
+    (typeof options === 'object' && !!options.vulgarFractions);
+
   const sFloorFinal = useVulgarFractions ? sFloor.trim() : sFloor;
 
-  // Handle infinitely repeating decimals next, since
-  // we'll never get an exact match for a switch case:
+  const getFraction = (vulgarFraction: VulgarFraction) =>
+    useVulgarFractions ? vulgarFraction : vulgarToPlainMap[vulgarFraction];
+
+  // Handle infinitely repeating decimals and floating point math quirks with
+  // `closeEnough` since we'll never get an exact match for a switch case
   if (closeEnough(dDecimal, 0.33)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅓' : '1/3'}`;
+    return `${sFloorFinal}${getFraction('⅓')}`;
   } else if (closeEnough(dDecimal, 0.66)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅔' : '2/3'}`;
+    return `${sFloorFinal}${getFraction('⅔')}`;
   } else if (closeEnough(dDecimal, 0.2)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅕' : '1/5'}`;
+    return `${sFloorFinal}${getFraction('⅕')}`;
   } else if (closeEnough(dDecimal, 0.4)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅖' : '2/5'}`;
+    return `${sFloorFinal}${getFraction('⅖')}`;
   } else if (closeEnough(dDecimal, 0.6)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅗' : '3/5'}`;
+    return `${sFloorFinal}${getFraction('⅗')}`;
   } else if (closeEnough(dDecimal, 0.8)) {
-    return `${sFloorFinal}${useVulgarFractions ? '⅘' : '4/5'}`;
+    return `${sFloorFinal}${getFraction('⅘')}`;
+  } else if (closeEnough(dDecimal, 0.166)) {
+    return `${sFloorFinal}${getFraction('⅙')}`;
+  } else if (closeEnough(dDecimal, 0.833)) {
+    return `${sFloorFinal}${getFraction('⅚')}`;
+  } else if (closeEnough(dDecimal, 0.143)) {
+    return `${sFloorFinal}${getFraction('⅐')}`;
+  } else if (closeEnough(dDecimal, 0.111)) {
+    return `${sFloorFinal}${getFraction('⅑')}`;
+  } else if (closeEnough(dDecimal, 0.1)) {
+    return `${sFloorFinal}${getFraction('⅒')}`;
   } else {
     switch (dDecimal) {
       case 0.125:
-        return `${sFloorFinal}${useVulgarFractions ? '⅛' : '1/8'}`;
+        return `${sFloorFinal}${getFraction('⅛')}`;
       case 0.25:
-        return `${sFloorFinal}${useVulgarFractions ? '¼' : '1/4'}`;
+        return `${sFloorFinal}${getFraction('¼')}`;
       case 0.375:
-        return `${sFloorFinal}${useVulgarFractions ? '⅜' : '3/8'}`;
+        return `${sFloorFinal}${getFraction('⅜')}`;
       case 0.5:
-        return `${sFloorFinal}${useVulgarFractions ? '½' : '1/2'}`;
+        return `${sFloorFinal}${getFraction('½')}`;
       case 0.625:
-        return `${sFloorFinal}${useVulgarFractions ? '⅝' : '5/8'}`;
+        return `${sFloorFinal}${getFraction('⅝')}`;
       case 0.75:
-        return `${sFloorFinal}${useVulgarFractions ? '¾' : '3/4'}`;
+        return `${sFloorFinal}${getFraction('¾')}`;
       case 0.875:
-        return `${sFloorFinal}${useVulgarFractions ? '⅞' : '7/8'}`;
+        return `${sFloorFinal}${getFraction('⅞')}`;
     }
   }
 
