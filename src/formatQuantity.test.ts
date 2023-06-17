5aa7d60ea49292905cf8da6a2872ff59a8643857
Jake Boone <jakeboone02@gmail.com>
2023-06-17 11:51:49 -0700

    Drop Vite in favor of Bun.serve

--- src/formatQuantity.test.ts
+++ src/formatQuantity.test.ts
@@ -2,9 +2,15 @@ import { formatQuantity, formatRomanNumerals } from './formatQuantity';
 import { formatQuantityTests } from './formatQuantityTests';
 
 for (const [description, expects] of Object.entries(formatQuantityTests)) {
-  it(description, () => {
+  describe(description, () => {
     for (const [quantity, result, options] of expects) {
-      expect(formatQuantity(quantity, options)).toBe(result);
+      it(`${quantity}${
+        typeof options === 'undefined'
+          ? ''
+          : ` with option ${JSON.stringify(options)}`
+      } should evaluate to ${result}`, () => {
+        expect(formatQuantity(quantity, options)).toBe(result);
+      });
     }
   });
 }
